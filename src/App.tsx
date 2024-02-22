8e729f83ade69abc8b179aa265f8e967d6152b14
unknown <farzad.jafarpour@gmail.com>
2024-02-22 16:49:33 +0330

    fixed login button conditional rendering issue

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
-import AuthLayout from "./components/layout/authLayout.tsx/AuthLayout";
-import AuthLayout2 from "./components/layout/authLayout2/AuthLayout2";
-import LoginForm from "./components/login form/loginform";
-import Register from "./components/register/register";
+import Changepsw from "./components/changepsw/changepsw";
 import Forgot from "./components/forgot/forgot";
 import Forgotmsg from "./components/forgotmsg/forgotmsg";
-import Changepsw from "./components/changepsw/changepsw";
-
+import AuthLayout from "./components/layout/authLayout.tsx/AuthLayout";
+import LoginForm from "./components/login form/loginform";
+import Register from "./components/register/register";
 
 export default function App() {
   return (
@@ -14,13 +12,10 @@ export default function App() {
       <Routes>
         <Route path="/" element={<AuthLayout />}>
           <Route path="login" element={<LoginForm />} />
-        </Route>
-        <Route path="/" element={<AuthLayout2 />}>
           <Route path="register" element={<Register />} />
           <Route path="forgot" element={<Forgot />} />
           <Route path="forgotmsg" element={<Forgotmsg />} />
           <Route path="changepsw" element={<Changepsw />} />
-          <Route path="m" element={<MyComponent />} />
         </Route>
       </Routes>
     </Router>
