f669414c5375d3659967bcccb0f3ac009969dc27
Arman Zavvari <armanzavvari@gmail.com>
2024-02-24 17:51:18 +0330

    fix priority co component

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,25 @@
 // import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
-// // import Changepsw from "./components/changepsw/changepsw";
-// // import Forgot from "./components/forgot/forgot";
-// // import Forgotmsg from "./components/forgotmsg/forgotmsg";
-// // import AuthLayout from "./components/layout/authLayout.tsx/AuthLayout";
-// // import LoginForm from "./components/login form/loginform";
-// // import Register from "./components/register/register";
+// import Changepsw from "./components/changepsw/changepsw";
+// import Forgot from "./components/forgot/forgot";
+// import Forgotmsg from "./components/forgotmsg/forgotmsg";
+// import AuthLayout from "./components/layout/authLayout.tsx/AuthLayout";
+// import LoginForm from "./components/login form/loginform";
+// import Register from "./components/register/register";
 
 // export default function App() {
 //   return (
-//     // <Router>
-//     //   <Routes>
-//     //     <Route path="/" element={<AuthLayout />}>
-//     //       <Route path="login" element={<LoginForm />} />
-//     //       <Route path="register" element={<Register />} />
-//     //       <Route path="forgot" element={<Forgot />} />
-//     //       <Route path="forgotmsg" element={<Forgotmsg />} />
-//     //       <Route path="changepsw" element={<Changepsw />} />
-//     //     </Route>
-//     //   </Routes>
-//     // </Router>
-//     <priorityCard></priorityCard>
+//     <Router>
+//       <Routes>
+//         <Route path="/" element={<AuthLayout />}>
+//           <Route path="login" element={<LoginForm />} />
+//           <Route path="register" element={<Register />} />
+//           <Route path="forgot" element={<Forgot />} />
+//           <Route path="forgotmsg" element={<Forgotmsg />} />
+//           <Route path="changepsw" element={<Changepsw />} />
+//         </Route>
+//       </Routes>
+//     </Router>
 //   );
 // }
 
 
-
-
-// app.tsx
-import React from "react";
-import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
-import PriorityCard from "../src/components/priority/priority"; // Import PriorityCard component
-
-export default function App() {
-  return (
-    <PriorityCard/>
-  );
-}
