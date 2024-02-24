0129ab1a5468120585377363053116296db42ff5
Matinchrz <matinch@yahoo.com>
2024-02-24 15:44:05 +0330

    sidebar added

--- src/App.tsx
+++ src/App.tsx
@@ -23,10 +23,5 @@ export default function App() {
         </Route>
       </Routes>
     </Router>
-
   );
 }
-
-
-
-
---
+++ src/components/common/sidebar/sidebar.tsx
@@ -0,0 +1,48 @@
+import React from "react";
+import { useLocation } from "react-router-dom";
+import Dashsidebar from "./dash sidebar/dashsidebar";
+import Profilesidebar from "./profile sidebar/profilesidebar";
+import icons from "../../../utils/icons/icons";
+
+const routeComponents: Record<string, React.FC> = {
+  "/dashh": Dashsidebar,
+  "/dash": Profilesidebar,
+};
+
+const SmartSidebar: React.FC = () => {
+  const location = useLocation();
+  const PageComponent = routeComponents[location.pathname];
+
+  return (
+    <div
+      dir="rtl"
+      className="flex flex-col justify-between item-center bg-white p-4 border-l-2 h-[100vh]"
+    >
+      <div className="flex flex-col">
+        <h1 className="text-transparent bg-clip-text bg-gradient-to-r from-teal-600 to-sky-400  font-extrabold text-4xl leading-[45.09px] text-center mt-5">
+          کوئرا تسک منیجر
+        </h1>
+        <div className="self-end my-10">
+          {PageComponent && <PageComponent />}
+        </div>
+      </div>
+      <div>
+        <div>
+          <button className="flex justify-start items-center gap-1 mb-5 mr-2">
+            <span className="sq1"></span>
+            <p>نیلوفر</p>
+          </button>
+        </div>
+        <div className="flex justify-between">
+          <button className="flex justify-center items-center gap-1 bg-white p-1 px-2 rounded-md text-gray-500">
+            {icons.door("#818181", "20px")}
+            خروج
+          </button>
+          <button>{icons.sun("gray", "20px")}</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SmartSidebar;
