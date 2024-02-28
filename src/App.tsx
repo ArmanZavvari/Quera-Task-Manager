9d21d8459d31b2b5820012cb82ed5efa541c4d36
Arman Zavvari <armanzavvari@gmail.com>
2024-02-28 12:15:00 +0330

    initial project

--- src/App.tsx
+++ src/App.tsx
@@ -1,59 +1,12 @@
-// App.tsx
-import React from 'react';
-import Filter from './components/filter/filter';
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import ListComponent from "./components/list/list"; // Make sure to capitalize component names
 
-const App = () => {
-  const tasks = [
-    { 
-      id: 1, 
-      date: '2024-02-27', 
-      tag: 'Work', 
-      member: 'John', 
-      priority: '' 
-    },
-    { 
-      id: 2, 
-      date: '2024-02-28', 
-      tag: 'Study', 
-      member: 'Alice', 
-      priority: 'Medium' 
-    },
-    { 
-      id: 3, 
-      date: '', // No date
-      tag: 'Project', 
-      member: 'Bob', 
-      priority: '' 
-    },
-    { 
-      id: 4, 
-      date: '2024-03-01', 
-      tag: 'Work', 
-      member: '', 
-      priority: 'High' 
-    },
-    { 
-      id: 5, 
-      date: '', // No date
-      tag: 'Study', 
-      member: 'John', 
-      priority: 'Medium' 
-    },
-    { 
-      id: 6, 
-      date: '2024-03-02', 
-      tag: 'Project', 
-      member: 'Bob', 
-      priority: '' 
-    },
-    // Add more tasks as needed
-  ];
-  
+export default function App() {
   return (
-    <div>
-      <Filter tasks={tasks} />
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/list" element={<ListComponent />} />
+      </Routes>
+    </Router>
   );
-};
-
-export default App;
+}
--- src/components/list/list.jsx
+++ src/components/list/list.jsx
@@ -3,8 +3,6 @@ import React from "react";
 const ListComponent = () => {
   return (
     <div className="flex">
-      <div className="inline-block w-16 h-16 rounded-md bg-pink-500">sccscscsc</div>
-
       <p>این یک تیتر برای این تسک است</p>
     </div>
   );
