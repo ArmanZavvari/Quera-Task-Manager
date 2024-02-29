b11b1e69330141c4cea59547734188827f669a8b
Arman Zavvari <armanzavvari@gmail.com>
2024-02-29 20:27:06 +0330

    initial list component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import ListComponent from "./components/list/list"; // Make sure to capitalize component names
+import SmartSidebar from "./components/common/sidebar/sidebar";
 
 export default function App() {
   return (
     <Router>
       <Routes>
         <Route path="/list" element={<ListComponent />} />
+        <Route path="/pro" element={<SmartSidebar />} />
+
       </Routes>
     </Router>
   );
--- src/components/list/list.jsx
+++ src/components/list/list.jsx
@@ -1,9 +1,20 @@
 import React from "react";
+import icons from "../../../src/utils/icons/icons";
 
 const ListComponent = () => {
+  const now = new Date();
+  const formattedDate = now.toLocaleDateString("Fa-Ir", { day: "numeric", month: "long" });
+  console.log(formattedDate);
+
   return (
-    <div className="flex">
-      <p>این یک تیتر برای این تسک است</p>
+    <div dir="rtl" className="flex items-center justify-between w-[986px] mt-10">
+      <div className="flex items-center">
+        <span className="w-4 h-4 rounded bg-pink-500"></span>
+        <p className="text-base leading-6 text-right">این یک تیتر برای این تسک است</p>
+      </div>
+      <p>{formattedDate}</p>
+      <span>{icons.flag("red", "20px")}</span>
+      <span>{icons.paragraph("#BDC0C6", "20px")}</span>
     </div>
   );
 };
