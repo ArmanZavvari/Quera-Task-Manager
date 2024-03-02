ef0b1044b65619539f1223d43f0fcf42d343d56e
Matinchrz <matinch@yahoo.com>
2024-03-02 17:41:08 +0330

    parent component added and some problems solved

--- src/components/common/sidebar/dash sidebar/components/newWorkSpace/colorWorkSpace/colorWorkSpace.tsx
+++ src/components/common/sidebar/dash sidebar/components/newWorkSpace/colorWorkSpace/colorWorkSpace.tsx
@@ -2,12 +2,19 @@ import React, { useState } from "react";
 import icons from "./../../../../../../../utils/icons/icons";
 
 interface Props {
-  onSubmit: (color: string) => void;
-  selectedName: string; // اضافه شده
-  selectedColor: string; // اضافه شده
+  data: {
+    name: string;
+    color: string;
+  };
+  setData: React.Dispatch<
+    React.SetStateAction<{
+      name: string;
+      color: string;
+    }>
+  >;
 }
 
-const ColorWorkSpace: React.FC<Props> = ({ onSubmit, selectedName }) => {
+const ColorWorkSpace: React.FC<Props> = ({ data, setData }) => {
   const colors = [
     "#4C6EF5",
     "#228BE6",
@@ -25,87 +32,65 @@ const ColorWorkSpace: React.FC<Props> = ({ onSubmit, selectedName }) => {
   ];
   const [selectedColor, setSelectedColor] = useState("");
   const [backColor, setBackColor] = useState("#7D828C");
-  const [isOpen, setIsOpen] = useState(true);
   const [textColor, setTextColor] = useState("white");
 
   const handleColorSelect = (color: string) => {
     setSelectedColor(color);
     setBackColor(color);
     setTextColor("black");
+    setData({ ...data, color: color });
   };
   const handleColorDisable = (color: string) => {
     setSelectedColor(color);
     setBackColor(color);
     setTextColor("white");
-  };
-
-  const handleSubmit = () => {
-    onSubmit(selectedColor);
-    setIsOpen(false);
-  };
-
-  const handleClose = () => {
-    setIsOpen(false);
+    setData({ ...data, color: color });
   };
 
   const renderInitials = (name: string) => {
-    return name
-      .split(" ")
-      .map((word) => word[0])
-      .join(" ")
-      .toUpperCase();
+    const words = name.split(" ");
+    const firstLetter = words[0][0];
+    const secondLetter = words[1] ? words[1][0] : "";
+
+    return `${firstLetter} ${secondLetter}`;
   };
 
-  return isOpen ? (
-    <div className="fixed inset-0 flex items-center justify-center bg-gray-800 bg-opacity-50">
-      <div className="bg-white p-8 rounded-xl w-[501px] h-[294px] flex flex-col justify-between items-stretch">
-        <div className="flex items-center justify-between gap-x-6 mb-5">
-          <button onClick={handleClose}>{icons.close("black", "20px")}</button>
-          <h2 className="text-2xl font-bold">انتخاب رنگ ورک اسپیس</h2>
-          <button onClick={handleClose}>{icons.back("black", "20px")}</button>
-        </div>
-        <div className="flex ">
-          <div className="">
-            {selectedName && (
-              <span
-                className="text-2xl font-bold w-[70px] h-[70px] text-center rounded-md flex items-center justify-center"
-                style={{ backgroundColor: backColor, color: textColor }}
-              >
-                {renderInitials(selectedName)}
-              </span>
-            )}
-          </div>
-          <div className="w-[300px] mr-4">
-            <p className="text-sm font-normal mb-3">رنگ ورک اسپیس</p>
-            <div className="flex flex-wrap items-center justify-start">
-              <button
-                onClick={() => handleColorDisable("#7D828C")}
-                className="ml-2 mt-2"
-              >
-                {icons.disable("gray", "20px")}
-              </button>
-
-              {colors.map((color, index) => (
-                <div
-                  key={index}
-                  onClick={() => handleColorSelect(color)}
-                  className={`w-5 h-5 rounded-lg cursor-pointer ml-2 mt-2  ${
-                    selectedColor === color ? `` : `border-white border-2`
-                  }`}
-                  style={{ backgroundColor: color }}
-                ></div>
-              ))}
-            </div>
-          </div>
-        </div>
-        <div className="mt-4 text-center mx-[22px]">
-          <button onClick={handleSubmit} className="button-nws py-2 w-full">
-            ادامه
+  return (
+    <div className="flex ">
+      <div className="">
+        {`${data.name}` && (
+          <span
+            className="text-2xl font-bold w-[70px] h-[70px] text-center rounded-md flex items-center justify-center"
+            style={{ backgroundColor: backColor, color: textColor }}
+          >
+            {renderInitials(`${data.name}`)}
+          </span>
+        )}
+      </div>
+      <div className="w-[300px] mr-4">
+        <p className="text-sm font-normal mb-3">رنگ ورک اسپیس</p>
+        <div className="flex flex-wrap items-center justify-start">
+          <button
+            onClick={() => handleColorDisable("#7D828C")}
+            className="ml-2 mt-2"
+          >
+            {icons.disable("gray", "20px")}
           </button>
+
+          {colors.map((color, index) => (
+            <div
+              key={index}
+              onClick={() => handleColorSelect(color)}
+              className={`w-5 h-5 rounded-lg cursor-pointer ml-2 mt-2  ${
+                selectedColor === color ? `` : `border-white border-2`
+              }`}
+              style={{ backgroundColor: color }}
+            ></div>
+          ))}
         </div>
       </div>
     </div>
-  ) : null;
+  );
 };
 
 export default ColorWorkSpace;
